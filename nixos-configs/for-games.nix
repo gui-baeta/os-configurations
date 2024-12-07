a063bff01e6612b3bd563e629b8f350269aba663
Guilherme Rocha Fontes <phineas.guifontes@gmail.com>
2024-12-07 06:50:26 +0000

    Complete refactor to use flakes

---
+++ nixos-configs/for-games.nix
@@ -0,0 +1,133 @@
+{ options, pkgs, ... }:
+
+{
+	# Java - For Steam
+	programs.java.enable = true;
+
+	# Steam
+	programs.steam = {
+		enable = true;
+		gamescopeSession.enable = true;
+
+		#remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+		#dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+	};
+
+	programs.gamemode.enable = true;
+
+	# Steam Package overrides
+	nixpkgs.config.packageOverrides = pkgs: {
+	steam = pkgs.steam.override {
+	  extraProfile = ''unset VK_ICD_FILENAMES'';
+	  extraPkgs = pkgs: with pkgs; [
+	    xorg.libXcursor
+	    xorg.libXi
+	    xorg.libXinerama
+	    xorg.libXScrnSaver
+	    libpng
+	    libpulseaudio
+	    libvorbis
+	    stdenv.cc.cc.lib
+	    libkrb5
+	    keyutils
+	    dxvk
+	  ];
+	};
+	};
+
+	# Extra Packages for Linking
+	programs.nix-ld.libraries = options.programs.nix-ld.libraries.default ++ (with pkgs;
+	  [ stdenv.cc.cc
+	    openssl
+	    xorg.libXcomposite
+	    xorg.libXtst
+	    xorg.libXrandr
+	    xorg.libXext
+	    xorg.libX11
+	    xorg.libXfixes
+	    libGL
+	    libva
+	    pipewire
+	    xorg.libxcb
+	    xorg.libXdamage
+	    xorg.libxshmfence
+	    xorg.libXxf86vm
+	    libelf
+
+	    # Required
+	    glib
+	    gtk2
+	    bzip2
+
+	    # Without these it silently fails
+	    xorg.libXinerama
+	    xorg.libXcursor
+	    xorg.libXrender
+	    xorg.libXScrnSaver
+	    xorg.libXi
+	    xorg.libSM
+	    xorg.libICE
+	    gnome2.GConf
+	    nspr
+	    nss
+	    cups
+	    libcap
+	    SDL2
+	    libusb1
+	    dbus-glib
+	    ffmpeg
+	    # Only libraries are needed from those two
+	    libudev0-shim
+
+	    # Verified games requirements
+	    xorg.libXt
+	    xorg.libXmu
+	    libogg
+	    libvorbis
+	    SDL
+	    SDL2_image
+	    glew110
+	    libidn
+	    tbb
+
+	    # Other things from runtime
+	    flac
+	    freeglut
+	    libjpeg
+	    libpng
+	    libpng12
+	    libsamplerate
+	    libmikmod
+	    libtheora
+	    libtiff
+	    pixman
+	    speex
+	    SDL_image
+	    SDL_ttf
+	    SDL_mixer
+	    SDL2_ttf
+	    SDL2_mixer
+	    libappindicator-gtk2
+	    libdbusmenu-gtk2
+	    libindicator-gtk2
+	    libcaca
+	    libcanberra
+	    libgcrypt
+	    libvpx
+	    librsvg
+	    xorg.libXft
+	    libvdpau
+	    gnome2.pango
+	    cairo
+	    atk
+	    gdk-pixbuf
+	    fontconfig
+	    freetype
+	    dbus
+	    alsa-lib
+	    expat
+	    # Needed for electron
+	    libdrm
+	    mesa
+	    libxkbcommon ]);
+}
