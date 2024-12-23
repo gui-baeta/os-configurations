14a86ae28b80a77b27da19bd90cbcab567342137
Guilherme Rocha Fontes <phineas.guifontes@gmail.com>
2024-12-23 21:30:22 +0000

    home: Implement base module
    This commit adds the base home-manager module - "default.nix".
    Flake integrates this module.

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     # NixOS official package source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,21 +19,42 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, nix-index-database, ... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.pen-and-paper = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        "${self}/modules/."
-        "${self}/hosts/pen-and-paper/configuration.nix"
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nix-index-database,
+      ...
+    }@inputs:
+    {
+      # Please replace my-nixos with your hostname
+      nixosConfigurations.pen-and-paper = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
+          # TODO Put things from configuration.nix in modules when possible
+          "${self}/modules/."
+          "${self}/hosts/pen-and-paper/configuration.nix"
 
-        nix-index-database.nixosModules.nix-index
-        { programs.nix-index-database.comma.enable = true; }
-      ];
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.guibaeta = import "${self}/modules/home/.";
 
-      specialArgs.flake-inputs = inputs;
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+
+          nix-index-database.nixosModules.nix-index
+          {
+            programs.nix-index-database.comma.enable = true;
+          }
+        ];
+
+        specialArgs.flake-inputs = inputs;
+      };
     };
-  };
 }
