1c3711588fe9d293eb49ecaa52fef52fb04d6a69
Guilherme Rocha Fontes <phineas.guifontes@gmail.com>
2025-03-05 23:33:01 +0000

    home: add ncspot theme; leave note in fonts module

--- modules/home/ncspot.nix
+++ modules/home/ncspot.nix
@@ -19,6 +19,24 @@
         "9" = "voldown 2";
         "0" = "volup 2";
       };
+      theme = {
+        background = "#1F2228";
+        primary = "#ABB2BF";
+        secondary = "#DCDCDC";
+        title = "#E5C07B";
+        playing = "#E5C07B";
+        playing_selected = "#E5C07B";
+        playing_bg = "#282C34";
+        highlight = "#C6CBD4";
+        highlight_bg = "#3B4048";
+        error = "#E06C75";
+        error_bg = "#1F2228";
+        statusbar = "#E5C07B";
+        statusbar_progress = "#ABB2BF";
+        statusbar_bg = "#2C323C";
+        cmdline = "#282C34";
+        cmdline_bg = "#E5C07B";
+      };
     };
   };
 }
